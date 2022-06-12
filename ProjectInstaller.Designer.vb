ccd77a5b046ba80ab6df3e6e6ea71959d3d535bd
Mark Meadows <42901465+swodaemkram@users.noreply.github.com>
2022-06-12 16:26:33 -0400

    Add files via upload

---
+++ ProjectInstaller.Designer.vb
@@ -0,0 +1,48 @@
+﻿<System.ComponentModel.RunInstaller(True)> Partial Class ProjectInstaller
+    Inherits System.Configuration.Install.Installer
+
+    'Installer overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Component Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Component Designer
+    'It can be modified using the Component Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Retention1 = New System.ServiceProcess.ServiceProcessInstaller()
+        Me.Retention = New System.ServiceProcess.ServiceInstaller()
+        '
+        'Retention1
+        '
+        Me.Retention1.Account = System.ServiceProcess.ServiceAccount.LocalSystem
+        Me.Retention1.Password = Nothing
+        Me.Retention1.Username = Nothing
+        '
+        'Retention
+        '
+        Me.Retention.ServiceName = "Retention"
+        Me.Retention.ServicesDependedOn = New String() {"Will delete files in the ""C:\ProgramData\Gallagher\Backup "" Directory Automaticly" &
+            " that ", "are 30 days old and older."}
+        Me.Retention.StartType = System.ServiceProcess.ServiceStartMode.Automatic
+        '
+        'ProjectInstaller
+        '
+        Me.Installers.AddRange(New System.Configuration.Install.Installer() {Me.Retention1, Me.Retention})
+
+    End Sub
+
+    Friend WithEvents Retention1 As ServiceProcess.ServiceProcessInstaller
+    Friend WithEvents Retention As ServiceProcess.ServiceInstaller
+End Class
---
+++ Retention.vb
@@ -0,0 +1,59 @@
+﻿Public Class Retention
+
+
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        Dim Timer1 As New System.Timers.Timer
+
+        AddHandler Timer1.Elapsed, AddressOf Timer1_Elapsed
+
+        With Timer1
+            .Interval = 1000
+            .Enabled = True
+        End With
+
+
+        Timer1.Start()
+        WriteLog(Me.ServiceName & " Started")
+
+    End Sub
+
+    Protected Overrides Sub OnStop()
+        ' Add code here to perform any tear-down necessary to stop your service.
+        WriteLog(Me.ServiceName & " Stopped")
+    End Sub
+
+
+    Private Sub WriteLog(ByVal strMessage As String)
+
+        Dim strPath As String, file As System.IO.StreamWriter
+        strPath = AppDomain.CurrentDomain.BaseDirectory & "\Retention.log"
+        file = New System.IO.StreamWriter(strPath, True)
+        file.WriteLine(strMessage)
+        file.Close()
+
+    End Sub
+
+    Private Sub Timer1_Elapsed(sender As Object, e As EventArgs)
+        For Each foundFile As String In My.Computer.FileSystem.GetFiles("C:\ProgramData\Gallagher\Backup\") ' Go through each file in the directory and add is to the lost box
+
+            Dim FileCreationTime = FileDateTime(foundFile)                                                  ' Get the creation date of each file in the Directory
+            Dim DateFileCreated As Date = FileDateTime(foundFile).Date                                      'What date was the file created on
+            Dim CurrentTimeNow As Date = Now                                                                'What time is it right now   
+            Dim DaysOld = DateDiff(DateInterval.Day, DateFileCreated, CurrentTimeNow)                       'How many days diffrents between today and the file creation date
+
+            Dim TheCurrentTime = Now.ToString("HH:mm")                                                      ' format the current time 
+            'WriteLog(Me.ServiceName & "Checking Directory")
+            If TheCurrentTime = "12:00" Then                                                                'Delete Files at this time of day 
+                Dim INTDaysOld As Integer = Convert.ToInt32(DaysOld)                                        'Convert day into integer so we can compare it with a number
+
+                If DaysOld > 30 Then                                                                        'If older then this many days 
+
+                    My.Computer.FileSystem.DeleteFile(foundFile)                                            'Delete the file
+
+                End If
+
+            End If
+
+        Next
+    End Sub
+End Class
